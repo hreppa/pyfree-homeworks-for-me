267e1f86d8872684c197958fa76fdcc0b61fe20e
Пётр <khreptugov@gmail.com>
2025-03-28 00:55:17 +0300

    Решение задачи сразу третий

--- homeworks/Modul_1/unit_1.py
+++ homeworks/Modul_1/unit_1.py
@@ -1,7 +1,20 @@
 '''Запрос даты'''
+def get_date():
+    date_fild = input('Введите КОГДА СДЕЛАТЬ - ')
+    return date_fild
+def get_task():
+    task_fild = input('Введите ЧТО СДЕЛАТЬ - ')
+    return task_fild
 
-date_fild = input('Введите КОГДА СДЕЛАТЬ - ')
 
-task = input('Введите ЧТО СДЕЛАТЬ - ')
+dates_tasks = {}
 
-print(f'Список дел: {date_fild} - {task}')
+for i in range(3):
+    date = get_date()
+    task = get_task()
+    dates_tasks[date] = task
+
+print('Список дел на 3 дня')
+for i in dates_tasks:
+    print(f'\t{i} - {dates_tasks[i]}')
+# print(f'Список дел: {date_fild} - {task}')
